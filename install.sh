14b33743db3c1eb4b25272f15c57488ca7c011b8
Zander Hill <Zander@civet.ws>
2014-02-16 21:48:56 -0500

    Setup install script

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-bin_dir=${1:-'~/bin/'}
+bin_dir=${1:-"${HOME}/bin/"}
 function download_file(){
-  curl -q https://raw.github.com/zph/sack/master/bin/${1} > "${bin_dir}${1}"
+  curl -s https://raw.github.com/zph/sack/master/bin/${1} > "${bin_dir}${1}"
+  echo "Installed ${1} into ${bin_dir}"
 }
 
 download_file 'sack'
