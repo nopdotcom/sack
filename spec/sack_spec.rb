2d5a0d193c1cfe755d0a1f4abb5f28319a583987
Zander Hill <Zander@civet.ws>
2014-02-23 03:20:17 -0500

    Improve good/bad data tests

--- spec/sack_spec.rb
+++ spec/sack_spec.rb
@@ -4,23 +4,43 @@ require 'minitest/mock'
 require 'minitest/pride'
 require 'mocha/mini_test'
 require_relative '../lib/sack.rb'
+require 'stringio'
 
 require 'dotenv'
 
 describe Sack do
   let(:bad_data) { File.read('fixtures/no_lines.out') }
+  let(:good_data) { File.read('fixtures/ruby_stack_trace.out') }
+
   describe '::Stdin' do
 
+    before do
+      module Kernel
+        def exit(arg)
+        end
+      end
+    end
+
     describe 'bad data' do
-      # it 'fails gracefully' do
-      #   Sack::Stdin.search(bad_data.split("\n"))
-      # end
 
-      # it 'exits if no bad data' do
-      #   Kernel.expects(:exit).with(1).at_least_once
-      #   Sack::Stdin.search(bad_data)
+      it 'exits if no bad data' do
+        Object.expects(:exit).at_least_once
+        Sack::Stdin.search(bad_data.split("\n"))
+      end
+    end
 
-      # end
+    describe 'good data' do
+      it 'doesnt exit if no bad data' do
+
+        old_stdout = STDOUT
+        STDOUT = StringIO.new
+
+        Object.expects(:exit).never
+        Sack::Stdin.search(good_data.split("\n"))
+
+        STDOUT = old_stdout
+      end
+
     end
     describe '#search' do
     end
